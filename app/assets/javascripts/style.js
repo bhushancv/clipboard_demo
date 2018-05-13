03461460cb570c266ed7c1df5e590f3fab98012e
bhushanjadhavinfo <jadhavbhushan30@gmail.com>
2018-05-13 17:41:04 +0530

    Changed website url and javascript

--- app/assets/javascripts/style.js
+++ app/assets/javascripts/style.js
@@ -1,7 +1,17 @@
 $(document).ready(function(){  
-  var clipboard = new Clipboard('.clipboard-btn');
-  $('.clipboard-btn').click(function(e) {
+  var btn = document.getElementById('clipboard-btn');
+  var clipboard = new Clipboard(btn);
+  
+  $('#clipboard-btn').click(function(e) {
     e.target.classList.add('copied');
     setTimeout(function() { e.target.classList.remove('copied'); }, 1500);
   });  
+
+  clipboard.on('success', function(e) {
+    console.log(e);
+  });
+
+  clipboard.on('error', function(e) {
+    console.log(e);
+  });
 });
